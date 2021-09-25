c1747456f08aa67a8478276be785cbb14cb91dda
Olubo Ifeoluwa <ifeoluwaolubo@gmail.com>
2021-09-25 12:02:54 +0100

    Started the implementation for test driven development in go

---
+++ day1/hello_test.go
@@ -0,0 +1,36 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	}
+
+	t.Run("saying hello to people", func (t *testing.T) {
+		got := Hello("world", "")
+		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func (t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Pogba", "French")
+		want := "Bonjour, Pogba"
+		assertCorrectMessage(t, got, want)
+	})
+}
